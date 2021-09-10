186b892aa3609a4bbed562c3038c34537ef8ad93
Gonzalo Marinelli <gonzalomarinelli16@gmail.com>
2021-09-10 20:13:48 -0300

    Primer archivo de JS

---
+++ js/main.js
@@ -0,0 +1,105 @@
+//Está no va a ser la funcionalidad final, por lo tanto el nombre del archivo (main.js), como de las funciones van a cambiar más adelante
+
+
+//Creo una constante JSON
+const productosMaiz = [{ "id": 1, "hibrido": "NS 7917", "nComparaciones": 71, "rindeKGxHA": 11.253, "tecnologia": "VT3P" },
+    { "id": 2, "hibrido": "AX 7761", "nComparaciones": 72, "rindeKGxHA": 12.859, "tecnologia": "VT3P" },
+    { "id": 3, "hibrido": "AX 852", "nComparaciones": 73, "rindeKGxHA": 11.758, "tecnologia": "MG RR2" },
+    { "id": 4, "hibrido": "NS 4619", "nComparaciones": 74, "rindeKGxHA": 12.035, "tecnologia": "INTACTA RR2 PROLIGATE" },
+    { "id": 5, "hibrido": "NS 4955", "nComparaciones": 75, "rindeKGxHA": 12.756, "tecnologia": "RESISTENTE AL GLIFOSATO" },
+    { "id": 6, "hibrido": "NS 5258", "nComparaciones": 76, "rindeKGxHA": 11.005, "tecnologia": "INTACTA RR2 PRO" }
+];
+//creo una funcion guardar local
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+//Guardo en el STORAGE local la lista de productos Maiz JSON
+guardarLocal("listaProductosMaiz", JSON.stringify(productosMaiz));
+//obtengo los productos almacenados de la listaProductosMaiz
+let almacenados = JSON.parse(localStorage.getItem("listaProductosMaiz"));
+//Variable que guarda el producto seleccionado para comparar
+let productoSeleccionado;
+//Guardo en una variable la sección donde se van a mostrar los productos
+let sectProducts = document.getElementById("sectProducts");
+//Muestro los productos ya guardados
+mostrarProductos();
+
+function mostrarProductos() {
+    //Actualizamos los productos almacenados de la listaProductosMaiz, por si hubo algun cambio (Más adelate).
+    almacenados = JSON.parse(localStorage.getItem("listaProductosMaiz"));
+    for (const producto of almacenados) {
+        sectProducts.innerHTML += `<!-- bloque Product ${producto.id} -->
+        <div class="col-4 sectProducts__div" id="product">
+            <div class="sectProducts__div__product">
+                <div class="sectProducts__div__product__header">
+                    <h2><strong>${producto.hibrido}</strong></h2>
+                    <img src="img/Icons/maiz.png" class="sect" alt="maiz">
+                </div>
+                <div class="sectProducts__div__product__body">
+                    <p>Tecnología</p>
+                    <p><strong>${producto.tecnologia}</strong></p>
+                </div>
+                <div class="sectProducts__div__product__footer">
+                    <button><strong>Ficha Técnica</strong></button>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <strong>Comparar con</strong>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        ${listarOtrosProductos(producto.id, almacenados)}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    }
+}
+
+function listarOtrosProductos(idProducto, arrayProductos) {
+    let acumulador = "";
+    for (const producto of arrayProductos) {
+        if (producto.id != idProducto) {
+            acumulador += `<li><a class="dropdown-item" onclick="comparar(${idProducto},${producto.id})">${producto.hibrido}</a></li>`;
+        }
+    }
+    return acumulador;
+}
+
+function comparar(idProducto, idTestigo) {
+    let table = document.getElementById("tabla");
+    if (table != null) {
+        table.parentNode.removeChild(table);
+    }
+    //Como recibimos el id del producto ahora hay que guardar el objeto producto en la variable que corresponde.
+    for (const product of almacenados) {
+        if (product.id == idProducto) {
+            var producto = product;
+        }
+    }
+    //Como recibimos el id del testigo ahora hay que guardar el objeto testigo en la variable que corresponde.
+    for (const testig of almacenados) {
+        if (testig.id == idTestigo) {
+            var testigo = testig;
+        }
+    }
+    sectProducts.innerHTML += `<table class="table table-bordered" id="tabla">
+                                    <thead>
+                                     <tr>
+                                        <th scope="col">Tu híbrido</th>
+                                        <th scope="col">Testigo</th>
+                                        <th scope="col">N°</th>
+                                        <th scope="col">Rinde tu híbrido (kg/ha)</th>
+                                        <th scope="col">Rinde testigo (kg/ha)</th>
+                                        <th scope="col">DIF</th>
+                                     </tr>
+                                    </thead>
+                                    <tbody>
+                                     <tr>
+                                        <th scope="row">${producto.hibrido}</th>
+                                        <td>${testigo.hibrido}</td>
+                                        <td>${producto.nComparaciones}</td>
+                                        <td>${producto.rindeKGxHA}</td>
+                                        <td>${testigo.rindeKGxHA}</td>
+                                        <td>${producto.rindeKGxHA - testigo.rindeKGxHA}</td>
+                                     </tr>
+                                    </tbody>
+                                 </table>`;
+}
