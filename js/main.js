289a702a4e800ad72bbfb38981c5168de655c56d
Gonzalo Marinelli <gonzalomarinelli16@gmail.com>
2021-09-28 00:00:49 -0300

    Correción del main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 //Varibale para guardar los productos provenientes del JSON
 let listaProductosJSON;
+
 //variables para trabajar con el DOM.
 let sectProducts = document.querySelector("#sectProducts");
 let table = document.querySelector("#tabla");
@@ -14,31 +15,58 @@ function mostrarProductos() {
     $.get('data/data.json', (respuesta, estado) => {
         listaProductosJSON = respuesta; //guardamos el json en una variable
         listaProductosJSON.map(prod => { //recorremos el array y utilizamos sus datos aplicandolos al dom.
-                sectProducts.innerHTML += `<!-- bloque Product ${prod.id} -->
-            <div class="col-4 sectProducts__div" id="product">
-                <div class="sectProducts__div__product">
-                    <div class="sectProducts__div__product__header">
-                        <h2><strong>${prod.hibrido}</strong></h2>
-                        <img src="img/Icons/maiz.png" class="sect" alt="maiz">
-                    </div>
-                    <div class="sectProducts__div__product__body">
-                        <p>Tecnología</p>
-                        <p><strong>${prod.tecnologia}</strong></p>
-                    </div>
-                    <div class="sectProducts__div__product__footer">
-                        <button><strong>Ficha Técnica</strong></button>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                            <strong>Comparar con</strong>
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            ${listarOtrosProductos(prod.id)} 
-                            </ul>
+                $("#sectProducts").prepend(`<!-- bloque Product ${prod.id} -->
+                    <div class="col-4 sectProducts__div" id="product">
+                        <div class="sectProducts__div__product">
+                            <div class="sectProducts__div__product__header">
+                                <h2><strong>${prod.hibrido}</strong></h2>
+                                <img src="img/Icons/maiz.png" class="sect" alt="maiz">
+                            </div>
+                            <div class="sectProducts__div__product__body">
+                                <p>Tecnología</p>
+                                <p><strong>${prod.tecnologia}</strong></p>
+                            </div>
+                            <div class="sectProducts__div__product__footer">
+                                <div class="dropdown">
+                                    <div class="btn-group dropend">
+                                        <button type="button" class="btn btn-secondary dropdown-toggle" onclick="consultarStock(${prod.id})" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <strong>Consultar stock</strong>
+                                        </button>
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="mostrarStock${prod.id}">
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <strong>Comparar con</strong>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    ${listarOtrosProductos(prod.id)} 
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>`;
+                    </div>`).slideDown("slow");
             }) //llamo a la función listarOtrosProductos mediante el id del producto seleccionado
+    })
+}
+
+function consultarStock(idProducto) {
+    //Url para el GET
+    let URLGET = "https://jsonplaceholder.typicode.com/todos/"
+    URLGET = URLGET + idProducto; //Preparamos el url con la id del "producto"
+    $("#mostrarStock" + idProducto).empty(); //Vaciamos el contenedor por si no es la primera vez que se aplica la función
+    let rta;
+    $.get(URLGET, function(respuesta, estado) {
+        if (estado === "success") {
+            let producto = respuesta;
+            if (producto.completed == true) {
+                rta = "Hay stock";
+            } else {
+                rta = "No hay stock";
+            }
+            return $("#mostrarStock" + idProducto).prepend(rta);
+        }
     })
 }
 
@@ -54,6 +82,7 @@ function listarOtrosProductos(idProducto) {
 }
 
 function comparar(idProducto, idTestigo) {
+    table = document.querySelector("#tabla");
     //Si la tabla ya fue creada la removemos
     let productoS; //ProductoSeleccionado
     let testigoS; //Testigo seleccionado
@@ -67,27 +96,26 @@ function comparar(idProducto, idTestigo) {
             testigoS = prod;
         }
     })
-    sectProducts.innerHTML += `<table class="table table-bordered" id="tabla" style="display: none">
-    <thead>
-     <tr>
-        <th scope="col">Tu híbrido</th>
-        <th scope="col">Testigo</th>
-        <th scope="col">N°</th>
-        <th scope="col">Rinde tu híbrido (kg/ha)</th>
-        <th scope="col">Rinde testigo (kg/ha)</th>
-        <th scope="col">DIF</th>
-     </tr>
-    </thead>
-    <tbody>
-     <tr>
-        <th scope="row">${productoS.hibrido}</th>
-        <td>${testigoS.hibrido}</td>
-        <td>${productoS.nComparaciones}</td>
-        <td>${productoS.rindeKGxHA}</td>
-        <td>${testigoS.rindeKGxHA}</td>
-        <td>${productoS.rindeKGxHA - testigoS.rindeKGxHA}</td>
-     </tr>
-    </tbody>
- </table>`;
-    $("#tabla").fadeIn("slow");
+    $("#sectProducts").append(`<table class="table table-bordered" id="tabla">
+        <thead>
+         <tr>
+             <th scope="col">Tu híbrido</th>
+             <th scope="col">Testigo</th>
+             <th scope="col">N°</th>
+             <th scope="col">Rinde tu híbrido (kg/ha)</th>
+             <th scope="col">Rinde testigo (kg/ha)</th>
+             <th scope="col">DIF</th>
+         </tr>
+        </thead>
+        <tbody>
+         <tr>
+             <th scope="row">${productoS.hibrido}</th>
+             <td>${testigoS.hibrido}</td>
+             <td>${productoS.nComparaciones}</td>
+             <td>${productoS.rindeKGxHA}</td>
+             <td>${testigoS.rindeKGxHA}</td>
+             <td>${productoS.rindeKGxHA - testigoS.rindeKGxHA}</td>
+         </tr>
+        </tbody>
+      </table>`).fadeIn("slow"); //Por alguna razón no hace la animación (?
 }
