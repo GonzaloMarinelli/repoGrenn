24f2941ab69e34eab4359133eb1e82dfc959de11
Gonzalo Marinelli <gonzalomarinelli16@gmail.com>
2021-10-13 01:21:08 -0300

    update fixes, pages productos

--- js/main.js
+++ js/main.js
@@ -2,80 +2,57 @@
 
 //Varibale para guardar los productos provenientes del JSON
 let listaProductosJSON;
-
+let tipoProd = sessionStorage.getItem('tipoProd');
 //variables para trabajar con el DOM.
 let sectProducts = document.querySelector("#sectProducts");
 let table = document.querySelector("#tabla");
 //Esto se ejecuta una vez cargado el DOM.
+let contadorCBox; //esta variable sirve para contabilizar por id cada cbox por separado, esto evita que se bugueen los dropdown menu y funcione el clickableInside 
 
-mostrarProductos();
-
-function mostrarProductos() {
-    //obtenemos el json
-    $.get('data/data.json', (respuesta, estado) => {
-        listaProductosJSON = respuesta; //guardamos el json en una variable
-        listaProductosJSON.map(prod => { //recorremos el array y utilizamos sus datos aplicandolos al dom.
-                $("#sectProducts").prepend(`<!-- bloque Product ${prod.id} -->
-                    <div class="col-4 sectProducts__div" id="product">
-                        <div class="sectProducts__div__product">
-                            <div class="sectProducts__div__product__header">
-                                <h2><strong>${prod.hibrido}</strong></h2>
-                                <img src="img/Icons/maiz.png" class="sect" alt="maiz">
-                            </div>
-                            <div class="sectProducts__div__product__body">
-                                <p>Tecnología</p>
-                                <p><strong>${prod.tecnologia}</strong></p>
-                            </div>
-                            <div class="sectProducts__div__product__footer">
-                                <div class="dropdown">
-                                    <div class="btn-group dropend">
-                                        <button type="button" class="btn btn-secondary dropdown-toggle" onclick="consultarStock(${prod.id})" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <strong>Consultar stock</strong>
+$.get('data/products.json', (respuesta, estado) => {
+    listaProductosJSON = respuesta; //guardamos el json en una variable
+    contadorCBox = 1; //Seteamos esta variable en 1 cada vez que listemos los productos 
+    var i = 1;
+    listaProductosJSON.map(prod => { //recorremos el array y utilizamos sus datos aplicandolos al dom.
+            if (prod.tipoProd == tipoProd) {
+                $("#sectProducts").prepend(`<!-- bloque Product ${i} -->
+                        <div class="col-4 sectProducts__div" id="product">
+                            <div class="sectProducts__div__product">
+                                <div class="sectProducts__div__product__header">
+                                    <h2><strong>${prod.hibrido}</strong></h2>
+                                    <img src="img/Icons/${prod.tipoProd}.png" class="sect" alt="maiz">
+                                </div>
+                                <div class="sectProducts__div__product__body">
+                                    <p>${prod.desc}</p>
+                                    <p><strong>${prod.info}</strong></p>
+                                </div>
+                                <div class="sectProducts__div__product__footer">
+                                <button><strong>Ficha Técnica</strong></button>
+                                    <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuClickableInside" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
+                                            <strong>Comparar con</strong>
                                         </button>
-                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" id="mostrarStock${prod.id}">
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuClickableInside">
+                                            ${listarOtrosProductos(prod.id)} 
+                                            <li><hr class="dropdown-divider"></li>
+                                            <li><a class="dropdown-item" >Listo</a></li>
                                         </ul>
                                     </div>
                                 </div>
-                                <div class="dropdown">
-                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <strong>Comparar con</strong>
-                                    </button>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    ${listarOtrosProductos(prod.id)} 
-                                    </ul>
-                                </div>
                             </div>
-                        </div>
-                    </div>`).slideDown("slow");
-            }) //llamo a la función listarOtrosProductos mediante el id del producto seleccionado
-    })
-}
-
-function consultarStock(idProducto) {
-    //Url para el GET
-    let URLGET = "https://jsonplaceholder.typicode.com/todos/"
-    URLGET = URLGET + idProducto; //Preparamos el url con la id del "producto"
-    $("#mostrarStock" + idProducto).empty(); //Vaciamos el contenedor por si no es la primera vez que se aplica la función
-    let rta;
-    $.get(URLGET, function(respuesta, estado) {
-        if (estado === "success") {
-            let producto = respuesta;
-            if (producto.completed == true) {
-                rta = "Hay stock";
-            } else {
-                rta = "No hay stock";
+                        </div>`).slideDown("slow");
+                i++;
             }
-            return $("#mostrarStock" + idProducto).prepend(rta);
-        }
-    })
-}
+        }) //llamo a la función listarOtrosProductos mediante el id del producto seleccionado
+})
 
-//Function para listar los otros productos no seleccionados en el btn comparar
+//funcion para listar los otros productos no seleccionados en el btn comparar con
 function listarOtrosProductos(idProducto) {
     let acumulador = "";
     listaProductosJSON.map(prod => {
-        if (prod.id != idProducto) {
-            acumulador += `<li><a class="dropdown-item" onclick="comparar(${idProducto},${prod.id})">${prod.hibrido}</a></li>`;
+        if (prod.id != idProducto && prod.tipoProd == tipoProd) {
+            acumulador += `<li class="dropdown-item"><span class="form-check"><input class="form-check-input" type="checkbox" id="cbox${contadorCBox}" value="${prod.id}"><label class="form-check-label" for="cbox${contadorCBox}">${prod.hibrido}</label></span></li>`;
+            contadorCBox++; //sumamos uno para diferenciar cada id con cbox + el numero de cbox
         }
     })
     return acumulador;
